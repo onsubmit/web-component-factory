3261c5ae960d1bc7345052310ef2de1731769a34
Andy <onsubmit@outlook.com>
2024-12-23 22:46:23 -0800

    Add getCustomElementConstructor

---
+++ src/components/getCustomElementConstructor.ts
@@ -0,0 +1,77 @@
+import { LifecycleName } from '../utils/webComponents';
+
+export function getCustomElementConstructor(input: {
+  attributes: Record<string, string>;
+  lifecycles: Map<LifecycleName, string>;
+  templateHtml: string;
+  mode: ShadowRootMode;
+}): CustomElementConstructor {
+  const { attributes, lifecycles, templateHtml, mode } = input;
+
+  return class extends HTMLElement {
+    static observedAttributes = [...Object.keys(attributes)];
+
+    private _shadowRoot: ShadowRoot;
+    private _templateHtml: string;
+    private _connected = false;
+    private _attributes = { ...attributes };
+
+    constructor() {
+      super();
+
+      this._templateHtml = templateHtml;
+
+      for (const attribute of this.attributes) {
+        this._attributes[attribute.name] = attribute.value;
+      }
+
+      this._shadowRoot = this.attachShadow({ mode });
+      this._shadowRoot.innerHTML = this._getInnerHtmlWithAttributes();
+    }
+
+    connectedCallback(): void {
+      this._executeLifecycleCallback('connected');
+      this._connected = true;
+    }
+
+    disconnectedCallback(): void {
+      this._executeLifecycleCallback('disconnected');
+    }
+
+    adoptedCallback(): void {
+      this._executeLifecycleCallback('adopted');
+    }
+
+    attributeChangedCallback(name: string, oldValue: string, newValue: string): void {
+      this._executeLifecycleCallback(
+        'attributeChanged',
+        ...[name, oldValue, newValue].map((v) => `'${v}'`),
+      );
+
+      if (!this._connected || oldValue === newValue) {
+        return;
+      }
+
+      this._attributes[name] = newValue;
+      this._shadowRoot.innerHTML = this._getInnerHtmlWithAttributes();
+    }
+
+    private _executeLifecycleCallback = (name: LifecycleName, ...args: any[]): void => {
+      const callback = lifecycles.get(name);
+      if (callback) {
+        eval(`${callback}\n;${name}Callback?.(${args.join(', ')});`);
+      }
+    };
+
+    private _getInnerHtmlWithAttributes = (): string => {
+      const div = document.createElement('div');
+      div.innerHTML = this._templateHtml;
+
+      for (const [key, value] of Object.entries(this._attributes)) {
+        div.innerHTML = div.innerHTML.replaceAll(`{${key}}`, value);
+      }
+
+      return div.innerHTML;
+    };
+  };
+}
--- src/components/getWebComponent.ts
+++ src/components/getWebComponent.ts
@@ -1,12 +1,13 @@
 import { getShadowRootModeOrThrow, isLifecycle, LifecycleName } from '../utils/webComponents';
+import { getCustomElementConstructor } from './getCustomElementConstructor';
 
 export function getWebComponent(element: Element, defaultMode: string): CustomElementConstructor {
-  const lifecycles = extractLifecycles();
-  const templateHtml = element.innerHTML;
-  const attributes = getAttributes();
-  const mode = getShadowRootModeOrThrow(
-    (element.getAttribute('#mode') || defaultMode).toLocaleLowerCase(),
-  );
+  return getCustomElementConstructor({
+    lifecycles: extractLifecycles(),
+    attributes: getAttributes(),
+    templateHtml: getTemplateHtml(),
+    mode: getShadowRootMode(),
+  });
 
   function extractLifecycles(): Map<LifecycleName, string> {
     const map = new Map<LifecycleName, string>();
@@ -37,70 +38,13 @@ export function getWebComponent(element: Element, defaultMode: string): CustomEl
     }, {});
   }
 
-  return class extends HTMLElement {
-    static observedAttributes = [...Object.keys(attributes)];
+  function getTemplateHtml(): string {
+    return element.innerHTML;
+  }
 
-    private _shadowRoot: ShadowRoot;
-    private _templateHtml: string;
-    private _connected = false;
-    private _attributes = { ...attributes };
-
-    constructor() {
-      super();
-
-      this._templateHtml = templateHtml;
-
-      for (const attribute of this.attributes) {
-        this._attributes[attribute.name] = attribute.value;
-      }
-
-      this._shadowRoot = this.attachShadow({ mode });
-      this._shadowRoot.innerHTML = this._getInnerHtmlWithAttributes();
-    }
-
-    connectedCallback(): void {
-      this._executeLifecycleCallback('connected');
-      this._connected = true;
-    }
-
-    disconnectedCallback(): void {
-      this._executeLifecycleCallback('disconnected');
-    }
-
-    adoptedCallback(): void {
-      this._executeLifecycleCallback('adopted');
-    }
-
-    attributeChangedCallback(name: string, oldValue: string, newValue: string): void {
-      this._executeLifecycleCallback(
-        'attributeChanged',
-        ...[name, oldValue, newValue].map((v) => `'${v}'`),
-      );
-
-      if (!this._connected || oldValue === newValue) {
-        return;
-      }
-
-      this._attributes[name] = newValue;
-      this._shadowRoot.innerHTML = this._getInnerHtmlWithAttributes();
-    }
-
-    private _executeLifecycleCallback = (name: LifecycleName, ...args: any[]): void => {
-      const callback = lifecycles.get(name);
-      if (callback) {
-        eval(`${callback}\n;${name}Callback?.(${args.join(', ')});`);
-      }
-    };
-
-    private _getInnerHtmlWithAttributes = (): string => {
-      const div = document.createElement('div');
-      div.innerHTML = this._templateHtml;
-
-      for (const [key, value] of Object.entries(this._attributes)) {
-        div.innerHTML = div.innerHTML.replaceAll(`{${key}}`, value);
-      }
-
-      return div.innerHTML;
-    };
-  };
+  function getShadowRootMode(): ShadowRootMode {
+    return getShadowRootModeOrThrow(
+      (element.getAttribute('#mode') || defaultMode).toLocaleLowerCase(),
+    );
+  }
 }
