cc155fb1d5bc9931330135df3c56f30061f9ded2
Andy <onsubmit@outlook.com>
2024-12-25 17:36:16 -0800

    Add markup describe

--- src/components/webComponentFactory.test.ts
+++ src/components/webComponentFactory.test.ts
@@ -7,181 +7,183 @@ import { WebComponentFactory } from './webComponentFactory';
 export type ErrorEventHandler = (this: Window, ev: ErrorEvent) => any;
 
 describe('WebComponentFactory', () => {
-  it('should render a basic component', async () => {
-    await fixture(html`
-      <wc-factory #mode="open">
-        <wc #name="my-paragraph-1" text="Hello world">
-          <p>{text}</p>
-        </wc>
-      </wc-factory>
-    `);
+  describe('markup', () => {
+    it('should render a basic component', async () => {
+      await fixture(html`
+        <wc-factory #mode="open">
+          <wc #name="my-paragraph-1" text="Hello world">
+            <p>{text}</p>
+          </wc>
+        </wc-factory>
+      `);
 
-    await fixture(html`<my-paragraph-1></my-paragraph-1>`);
-    await screen.findByShadowText('Hello world');
-  });
+      await fixture(html`<my-paragraph-1></my-paragraph-1>`);
+      await screen.findByShadowText('Hello world');
+    });
 
-  it('should override attributes', async () => {
-    await fixture(html`
-      <wc-factory #mode="open">
-        <wc #name="my-paragraph-2" text="Hello world">
-          <p>{text}</p>
-        </wc>
-      </wc-factory>
-    `);
+    it('should override attributes', async () => {
+      await fixture(html`
+        <wc-factory #mode="open">
+          <wc #name="my-paragraph-2" text="Hello world">
+            <p>{text}</p>
+          </wc>
+        </wc-factory>
+      `);
 
-    await fixture(html`<my-paragraph-2 text="Foo bar baz and so on"></my-paragraph-2>`);
-    await screen.findByShadowText('Foo bar baz and so on');
-  });
+      await fixture(html`<my-paragraph-2 text="Foo bar baz and so on"></my-paragraph-2>`);
+      await screen.findByShadowText('Foo bar baz and so on');
+    });
 
-  it('should hook into lifecycle callbacks', async () => {
-    const spy = vi.spyOn(console, 'log');
+    it('should hook into lifecycle callbacks', async () => {
+      const spy = vi.spyOn(console, 'log');
 
-    await fixture(html`
-      <wc-factory #mode="open">
-        <wc #name="my-paragraph-3" text="Hello world">
-          <p>{text}</p>
-          <script type="lifecycle" callback="connected">
-            function connectedCallback() {
-              debugger;
-              console.log('connectedCallback');
-            }
-          </script>
-          <script type="lifecycle" callback="attributeChanged">
-            function attributeChangedCallback(name, oldValue, newValue) {
-              console.log('attributeChangedCallback');
-              console.log('name: ' + name);
-              console.log('oldValue: ' + oldValue);
-              console.log('newValue: ' + newValue);
-            }
-          </script>
-        </wc>
-      </wc-factory>
-    `);
+      await fixture(html`
+        <wc-factory #mode="open">
+          <wc #name="my-paragraph-3" text="Hello world">
+            <p>{text}</p>
+            <script type="lifecycle" callback="connected">
+              function connectedCallback() {
+                debugger;
+                console.log('connectedCallback');
+              }
+            </script>
+            <script type="lifecycle" callback="attributeChanged">
+              function attributeChangedCallback(name, oldValue, newValue) {
+                console.log('attributeChangedCallback');
+                console.log('name: ' + name);
+                console.log('oldValue: ' + oldValue);
+                console.log('newValue: ' + newValue);
+              }
+            </script>
+          </wc>
+        </wc-factory>
+      `);
 
-    await fixture(html`<my-paragraph-3 text="How are you?"></my-paragraph-3>`);
-    await screen.findByShadowText('How are you?');
+      await fixture(html`<my-paragraph-3 text="How are you?"></my-paragraph-3>`);
+      await screen.findByShadowText('How are you?');
 
-    expect(spy).toHaveBeenCalledTimes(5);
-    expect(spy).toHaveBeenNthCalledWith(1, 'attributeChangedCallback');
-    expect(spy).toHaveBeenNthCalledWith(2, 'name: text');
-    expect(spy).toHaveBeenNthCalledWith(3, 'oldValue: null');
-    expect(spy).toHaveBeenNthCalledWith(4, 'newValue: How are you?');
-    expect(spy).toHaveBeenNthCalledWith(5, 'connectedCallback');
-  });
+      expect(spy).toHaveBeenCalledTimes(5);
+      expect(spy).toHaveBeenNthCalledWith(1, 'attributeChangedCallback');
+      expect(spy).toHaveBeenNthCalledWith(2, 'name: text');
+      expect(spy).toHaveBeenNthCalledWith(3, 'oldValue: null');
+      expect(spy).toHaveBeenNthCalledWith(4, 'newValue: How are you?');
+      expect(spy).toHaveBeenNthCalledWith(5, 'connectedCallback');
+    });
 
-  it('should use a closed shadow root mode', async () => {
-    // wc-factory defaults to "closed"
-    await fixture(html`
-      <wc-factory>
-        <wc #name="my-paragraph-4" text="Hello world">
-          <p>{text}</p>
-        </wc>
-      </wc-factory>
-    `);
+    it('should use a closed shadow root mode', async () => {
+      // wc-factory defaults to "closed"
+      await fixture(html`
+        <wc-factory>
+          <wc #name="my-paragraph-4" text="Hello world">
+            <p>{text}</p>
+          </wc>
+        </wc-factory>
+      `);
 
-    await fixture(html`<my-paragraph-4></my-paragraph-4>`);
-    const p = screen.queryByShadowText('Hello world');
-    expect(p).toBeNull();
-  });
+      await fixture(html`<my-paragraph-4></my-paragraph-4>`);
+      const p = screen.queryByShadowText('Hello world');
+      expect(p).toBeNull();
+    });
 
-  it('ignores children other than <wc> tags', async () => {
-    await fixture(html`
-      <wc-factory #mode="open">
-        <wc #name="my-paragraph-5" text="Hello world">
-          <p>{text}</p>
-        </wc>
-        <b>I'm ignored</b>
-      </wc-factory>
-    `);
+    it('ignores children other than <wc> tags', async () => {
+      await fixture(html`
+        <wc-factory #mode="open">
+          <wc #name="my-paragraph-5" text="Hello world">
+            <p>{text}</p>
+          </wc>
+          <b>I'm ignored</b>
+        </wc-factory>
+      `);
 
-    await fixture(html`<my-paragraph-5></my-paragraph-5>`);
-    await screen.findByShadowText('Hello world');
+      await fixture(html`<my-paragraph-5></my-paragraph-5>`);
+      await screen.findByShadowText('Hello world');
 
-    const b = screen.queryByText("I'm ignored");
-    expect(b).toBeNull();
-  });
+      const b = screen.queryByText("I'm ignored");
+      expect(b).toBeNull();
+    });
 
-  it('should throw when #name attribute is missing', async () => {
-    let errorEvent: ErrorEvent | undefined;
+    it('should throw when #name attribute is missing', async () => {
+      let errorEvent: ErrorEvent | undefined;
 
-    const errorEventHandler: ErrorEventHandler = (event) => {
-      event.preventDefault();
-      errorEvent = event;
-    };
+      const errorEventHandler: ErrorEventHandler = (event) => {
+        event.preventDefault();
+        errorEvent = event;
+      };
 
-    const obj = { errorEventHandler };
-    const spy = vi.spyOn(obj, 'errorEventHandler');
-    window.addEventListener('error', obj.errorEventHandler);
+      const obj = { errorEventHandler };
+      const spy = vi.spyOn(obj, 'errorEventHandler');
+      window.addEventListener('error', obj.errorEventHandler);
 
-    await fixture(html`
-      <wc-factory #mode="closed">
-        <wc text="Hello world">
-          <p>{text}</p>
-        </wc>
-      </wc-factory>
-    `);
+      await fixture(html`
+        <wc-factory #mode="closed">
+          <wc text="Hello world">
+            <p>{text}</p>
+          </wc>
+        </wc-factory>
+      `);
 
-    expect(spy).toHaveBeenCalled();
-    invariant(errorEvent);
-    expect(errorEvent.message).toBe('"#name" attribute is required');
+      expect(spy).toHaveBeenCalled();
+      invariant(errorEvent);
+      expect(errorEvent.message).toBe('"#name" attribute is required');
 
-    window.removeEventListener('error', obj.errorEventHandler);
-  });
+      window.removeEventListener('error', obj.errorEventHandler);
+    });
 
-  it('should throw when duplicate child component definitions exist', async () => {
-    let errorEvent: ErrorEvent | undefined;
+    it('should throw when duplicate child component definitions exist', async () => {
+      let errorEvent: ErrorEvent | undefined;
 
-    const errorEventHandler: ErrorEventHandler = (event) => {
-      event.preventDefault();
-      errorEvent = event;
-    };
+      const errorEventHandler: ErrorEventHandler = (event) => {
+        event.preventDefault();
+        errorEvent = event;
+      };
 
-    const obj = { errorEventHandler };
-    const spy = vi.spyOn(obj, 'errorEventHandler');
-    window.addEventListener('error', obj.errorEventHandler);
+      const obj = { errorEventHandler };
+      const spy = vi.spyOn(obj, 'errorEventHandler');
+      window.addEventListener('error', obj.errorEventHandler);
 
-    await fixture(html`
-      <wc-factory #mode="closed">
-        <wc #name="my-paragraph-6" text="Hello world">
-          <p>{text}</p>
-        </wc>
-        <wc #name="my-paragraph-6" text="Good morning">
-          <p>{text}</p>
-        </wc>
-      </wc-factory>
-    `);
+      await fixture(html`
+        <wc-factory #mode="closed">
+          <wc #name="my-paragraph-6" text="Hello world">
+            <p>{text}</p>
+          </wc>
+          <wc #name="my-paragraph-6" text="Good morning">
+            <p>{text}</p>
+          </wc>
+        </wc-factory>
+      `);
 
-    expect(spy).toHaveBeenCalled();
-    invariant(errorEvent);
-    expect(errorEvent.message).toBe('Duplicate definition found for "my-paragraph-6"');
+      expect(spy).toHaveBeenCalled();
+      invariant(errorEvent);
+      expect(errorEvent.message).toBe('Duplicate definition found for "my-paragraph-6"');
 
-    window.removeEventListener('error', obj.errorEventHandler);
-  });
+      window.removeEventListener('error', obj.errorEventHandler);
+    });
 
-  it('should expose the child component constructors', async () => {
-    await fixture(html`
-      <wc-factory #mode="open">
-        <wc #name="my-paragraph-7" text="Hello world">
-          <p>{text}</p>
-        </wc>
-      </wc-factory>
-    `);
+    it('should expose the child component constructors', async () => {
+      await fixture(html`
+        <wc-factory #mode="open">
+          <wc #name="my-paragraph-7" text="Hello world">
+            <p>{text}</p>
+          </wc>
+        </wc-factory>
+      `);
 
-    await fixture(html`<my-paragraph-7></my-paragraph-7>`);
-    await screen.findByShadowText('Hello world');
+      await fixture(html`<my-paragraph-7></my-paragraph-7>`);
+      await screen.findByShadowText('Hello world');
 
-    const factory = document.querySelector<WebComponentFactory>('wc-factory');
-    invariant(factory);
+      const factory = document.querySelector<WebComponentFactory>('wc-factory');
+      invariant(factory);
 
-    const component = factory.getChildComponent('my-paragraph-7');
-    invariant(component);
+      const component = factory.getChildComponent('my-paragraph-7');
+      invariant(component);
 
-    expect(component.mode).toBe('open');
+      expect(component.mode).toBe('open');
 
-    const paragraph = new component.constructor();
-    paragraph.setAttribute('text', 'Merry Christmas!');
-    document.body.appendChild(paragraph);
+      const paragraph = new component.constructor();
+      paragraph.setAttribute('text', 'Merry Christmas!');
+      document.body.appendChild(paragraph);
 
-    await screen.findByShadowText('Merry Christmas!');
+      await screen.findByShadowText('Merry Christmas!');
+    });
   });
 });
